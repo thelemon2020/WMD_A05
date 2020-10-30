e9791be61d28ed265b24ec854ad377df521e104f
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-10-30 15:37:16 -0400

    added more commands and separate thread for checking for new commands from server

--- A05/A05/Command.cs
+++ A05/A05/Command.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace A05
@@ -16,32 +17,27 @@ namespace A05
         public IPAddress serverIP { get; set; }
         public int serverPort { get; set; }
         public StringBuilder protocol { get; set; }
+        public Mutex waitToWrite { get; set; }
         
         public string ExecuteCommand()
         {
-            TcpClient server = new TcpClient();
-            server.Connect(serverIP, serverPort);// establish a connection with the server and get the data stream
-            NetworkStream serverStream = server.GetStream();
-            SendToServer(serverStream);
-            string serverResponse = ReceiveFromServer(serverStream);
-            serverStream.Close();
-            server.Close();
-            return serverResponse;
-        }
-        public void SendToServer(NetworkStream serverStream)
-        {
-            byte[] dataSent = Encoding.ASCII.GetBytes(protocol.ToString()); // package the data to send into bytes for the stream           
-            serverStream.Write(dataSent, 0, dataSent.Length); // write the bytes to the stream
-        }
-
-        public string ReceiveFromServer(NetworkStream serverStream)
-        {
-            string recievedData = null;
-            byte[] rawData = null;
-            rawData = new byte[1024]; // buffer that the bytes of response data are stored into from the stream read
-            int bytesRec = serverStream.Read(rawData, 0, rawData.Length); // need to know how many bytes were received to decode to ascii
-            recievedData += Encoding.ASCII.GetString(rawData, 0, bytesRec);// decode the response to a string
-            return recievedData; // return the response
+            NetworkStream serverStream = null;
+            StringBuilder serverResponse = new StringBuilder();
+            waitToWrite.WaitOne();
+            serverStream = InteractWithServer.connectToServer(serverIP, serverPort);
+            if (serverStream == null)
+            {
+                serverResponse.Append(command);
+                serverResponse.Append(" Failed - Server Could Not Be Reached");
+            }
+            else
+            {
+                InteractWithServer.writeToServer(serverStream, protocol.ToString());
+                serverResponse.Append(InteractWithServer.readFromServer(serverStream));
+                InteractWithServer.closeConnection(serverStream);
+            }
+            waitToWrite.ReleaseMutex();
+            return serverResponse.ToString();
         }
     }
 
--- A05/A05/MainWindow.xaml.cs
+++ A05/A05/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,31 +26,104 @@ namespace A05
     {
         public bool isConnected { get; set; }
         public connection currentConnection { get; set; }
+
+        public Mutex canWrite { get; set; }
+
+
         public MainWindow()
         {
+            canWrite = new Mutex();
             InitializeComponent();
         }
 
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
-            
+
             ConnectWindow newConnection = new ConnectWindow();
             newConnection.Owner = this;
             newConnection.ShowDialog();
-            currentConnection = new connection(newConnection.username, newConnection.userPassword, 
-                newConnection.ipAddress, newConnection.port);
-            if (currentConnection !=null)
+            currentConnection = new connection(newConnection.username, newConnection.userPassword,
+                newConnection.ipAddress, newConnection.port, canWrite);
+            if (currentConnection != null)
             {
-                StartConnectionCommand startUp = new StartConnectionCommand(currentConnection);
-                if (startUp.ExecuteCommand() != null)
+                StartConnectionCommand startUp = new StartConnectionCommand(currentConnection, canWrite);
+                string serverResponse = startUp.ExecuteCommand();
+                if (serverResponse != "")
                 {
                     isConnected = true;
 
                     string connectedMessage = string.Format("Connected to server at {0}", currentConnection.ipAddress.ToString());
-                    chatWindow.Text+=connectedMessage;
+                    chatWindow.Text += connectedMessage;
+                    userList.Text += currentConnection.username;
+                    userInput.IsEnabled = true;
+                    SubmitMessage.IsEnabled = true;
+                    ParameterizedThreadStart listenStartThread = new ParameterizedThreadStart(listenForMessage);
+                    Thread listenThread = new Thread(listenStartThread);
+                    object[] paramObj = new object[2];
+                    listenThread.Start(currentConnection.ipAddress);
+                }
+                else
+                {
+                    chatWindow.Text += serverResponse;
                 }
             }
             newConnection.Close();
+        }
+
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            if (userInput.Text != "")
+            {
+                SendMessageCommand messageToSend = new SendMessageCommand(currentConnection, userInput.Text, canWrite);
+                messageToSend.ExecuteCommand();
+            }
+        }
+
+        private void listenForMessage(object obj)
+        {
+            connection server = (connection)obj;
+            while (isConnected == true)
+            {
+                canWrite.WaitOne();
+                NetworkStream serverStream = null;
+                serverStream = InteractWithServer.connectToServer(currentConnection.ipAddress, currentConnection.serverPort);
+                string serverResponse = InteractWithServer.readFromServer(serverStream);
+                string[] arguments = serverResponse.Split(',');
+                AckCommand sendBack = null;
+                if (arguments[0] == "REPLY")
+                {
+                    userInput.Text += arguments[1];
+                    sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
+                    InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
+                }
+                else if (arguments[0] == "DISCONNECT")
+                {
+                    sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
+                    InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
+                    shutDownServer();
+                }
+                else
+                {
+                    sendBack = new AckCommand(currentConnection, "ACK,FAIL,<EOF>");
+                    InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
+                }
+                InteractWithServer.closeConnection(serverStream);
+                canWrite.ReleaseMutex();
+                Thread.Sleep(5000);
+            }
+        }
+        private void disconnectFromServer(object sender, RoutedEventArgs e)
+        {
+            shutDownServer();
+        }
+        public void shutDownServer()
+        {
+            isConnected = false;
+            currentConnection = null;
+            userInput.Text = "";
+            userInput.IsEnabled = false;
+            MenuDisconnect.IsEnabled = false;
+            userList.Text = "";
         }
     }
 }
