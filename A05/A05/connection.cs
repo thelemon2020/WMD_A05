847df45495ef0581d656ad149a5266bd7045fc84
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-10-29 21:03:34 -0400

    created basic client UI and ability to interface with server

---
+++ A05/A05/connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace A05
+{
+    public class connection
+    {
+        public string username { get; set; }
+        public string userPassword { get; set; }
+        public IPAddress ipAddress { get; set; }
+        public int serverPort { get; set; }
+
+        public connection(string user, string pass, string address, int port)
+        {
+            username = user;
+            userPassword = pass;
+            try
+            {
+                ipAddress = IPAddress.Parse(address);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Invalid IP Address Format", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            serverPort = port;
+        }
+    }
+}
