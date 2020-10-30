770c55449dca2e4cb209d25f8d366e751484b999
Tangent1al <y.dydxx.b@gmail.com>
2020-10-29 20:50:17 -0400

    Updates

--- A05/Server/ConnectRepo.cs
+++ A05/Server/ConnectRepo.cs
@@ -5,35 +5,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using System.Collections.Concurrent;
 
 namespace Server
 {
-    class ConnectRepo
+    public class ConnectRepo
     {
-        public Dictionary<string, TcpClient> repo;
+        public ConcurrentDictionary<string, TcpClient> repo;
 
         public ConnectRepo()
         {
-            repo = new Dictionary<string, TcpClient>();
+            repo = new ConcurrentDictionary<string, TcpClient>();
         }
 
         public void Add(string key, TcpClient client)
         {
-            repo.Add(key, client);
+            repo.TryAdd(key, client);
         }
 
 
         public void Remove(string key)
         {
-            repo.Remove(key);
+            repo.TryRemove(key, out TcpClient client);
         }
 
         public bool CheckExists(string key)
         {
-            if(repo.TryGetValue(key, out TcpClient tmpClient))
-            {
-                return true;
-            }
+            if(repo.TryGetValue(key, out TcpClient tmpClient)) { return true; }
             else { return false; }
         }
     }
--- A05/Server/ManageConnection.cs
+++ A05/Server/ManageConnection.cs
@@ -3,25 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Server
 {
     public class ManageConnection
     {
+        private ConnectRepo repo;
+
+        public ManageConnection()
+        {
+            repo = new ConnectRepo();
+        }
+
         public void Connect(TcpListener listener)
         {
             TcpClient client = listener.AcceptTcpClient();
+            Thread clientThread = new Thread(() => HandleClient(client, repo));
+            clientThread.Start();
         }
 
-        private bool CheckConnectionExists(ConnectRepo repo, Connection client)
+        private bool CheckConnectionExists(ConnectRepo repo, Command client)
         {
             return repo.CheckExists(client.Name);
         }
 
-        private void HandleClient()
+        public void HandleClient(TcpClient client, ConnectRepo repo)
         {
 
+            return;
         }
     }
 }
