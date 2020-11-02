1cbeac6181614a56405bc631c36f82f6fdf7c510
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-11-02 13:29:30 -0500

    Added new server interactions and commands for client

--- A05/A05/MainWindow.xaml.cs
+++ A05/A05/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.Hosting;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading;
@@ -38,33 +39,35 @@ namespace A05
 
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
-
             ConnectWindow newConnection = new ConnectWindow();
             newConnection.Owner = this;
             newConnection.ShowDialog();
-            currentConnection = new connection(newConnection.username, newConnection.userPassword,
-                newConnection.ipAddress, newConnection.port, canWrite);
-            if (currentConnection != null)
+            if (newConnection.canProceed == true)
             {
-                StartConnectionCommand startUp = new StartConnectionCommand(currentConnection, canWrite, currentConnection.ipAddress);
-                string serverResponse = startUp.ExecuteCommand();
-                if (serverResponse != "")
+                currentConnection = new connection(newConnection.username, newConnection.userPassword,
+                    newConnection.ipAddress, newConnection.port, canWrite);
+                if (currentConnection != null)
                 {
-                    isConnected = true;
+                    StartConnectionCommand startUp = new StartConnectionCommand(currentConnection, canWrite, currentConnection.ipAddress);
+                    string serverResponse = startUp.ExecuteCommand();
+                    if (serverResponse != "")
+                    {
+                        isConnected = true;
 
-                    string connectedMessage = string.Format("Connected to server at {0}", currentConnection.ipAddress.ToString());
-                    chatWindow.Text += connectedMessage;
-                    userList.Text += currentConnection.username;
-                    userInput.IsEnabled = true;
-                    SubmitMessage.IsEnabled = true;
-                    ParameterizedThreadStart listenStartThread = new ParameterizedThreadStart(listenForMessage);
-                    Thread listenThread = new Thread(listenStartThread);
-                    object[] paramObj = new object[2];
-                    listenThread.Start(currentConnection);
-                }
-                else
-                {
-                    chatWindow.Text += serverResponse;
+                        string connectedMessage = string.Format("Connected to server at {0}", currentConnection.ipAddress.ToString());
+                        chatWindow.Text += connectedMessage;
+                        userList.Text += currentConnection.username;
+                        userInput.IsEnabled = true;
+                        SubmitMessage.IsEnabled = true;
+                        ParameterizedThreadStart listenStartThread = new ParameterizedThreadStart(listenForMessage);
+                        Thread listenThread = new Thread(listenStartThread);
+                        object[] paramObj = new object[2];
+                        listenThread.Start(currentConnection);
+                    }
+                    else
+                    {
+                        chatWindow.Text += serverResponse;
+                    }
                 }
             }
             newConnection.Close();
@@ -89,27 +92,49 @@ namespace A05
                 serverStream = InteractWithServer.connectToServer(currentConnection.ipAddress, currentConnection.serverPort);
                 string serverResponse = InteractWithServer.readFromServer(serverStream);
                 string[] arguments = serverResponse.Split(',');
-                AckCommand sendBack = null;
-                if (arguments[0] == "REPLY")
-                {
-                    userInput.Text += arguments[1];
-                    sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
-                    InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
-                }
-                else if (arguments[0] == "DISCONNECT")
-                {
-                    sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
-                    InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
-                    shutDownServer();
-                }
-                else
-                {
-                    sendBack = new AckCommand(currentConnection, "ACK,FAIL,<EOF>");
-                    InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
-                }
+                parseResponse(arguments, serverStream);
                 InteractWithServer.closeConnection(serverStream);
                 canWrite.ReleaseMutex();
                 Thread.Sleep(5000);
+            }
+        }
+
+        private void parseResponse(string[] arguments, NetworkStream serverStream)
+        {
+            AckCommand sendBack = null;
+            if (arguments[0] == "REPLY")
+            {
+                userInput.Text += arguments[1];
+                sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
+                InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
+            }
+            else if (arguments[0] == "DISCONNECT")
+            {
+                sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
+                InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
+                shutDownServer();
+            }
+            else if(arguments[0] == "ADD")
+            {
+                string[] list = userList.Text.ToString().Split('\n');
+                list.Append(arguments[1] + '\n');
+                Array.Sort(list);
+                userList.Text = list.ToString();
+                sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
+                InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
+            }
+            else if (arguments[0] == "REMOVE")
+            {
+                string list = userList.Text.ToString();
+                list.Replace(arguments[1], "");
+                userList.Text = list;
+                sendBack = new AckCommand(currentConnection, "ACK,OK,<EOF>");
+                InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
+            }
+            else
+            {
+                sendBack = new AckCommand(currentConnection, "ACK,FAIL,<EOF>");
+                InteractWithServer.writeToServer(serverStream, sendBack.protocol.ToString());
             }
         }
         private void disconnectFromServer(object sender, RoutedEventArgs e)
